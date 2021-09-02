a0221b5e100dc2a58114a1526b2fe2af51de8089
Maryna Cherniavska <maryna.cherniavska@elastic.co>
2021-09-02 14:18:37 +0200

    some manual fixes to generated API

--- command.sh
+++ command.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-openapi-generator generate -i workplace-search.json -g kotlin --group-id org.elasticsearch.workplace-search --artifact-id java-api --invoker-package org.elasticsearch.workplacesearch --remove-operation-id-prefix --log-to-stderr -v
+openapi-generator generate -i workplace-search.json -g kotlin --group-id org.elasticsearch.workplace-search --artifact-id kotlin-api --invoker-package org.elasticsearch.workplacesearch --remove-operation-id-prefix --log-to-stderr -v
